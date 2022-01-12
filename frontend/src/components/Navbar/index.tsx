04035e9a3f8c57ecdd06005fea0cae571afb8319
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-01-12 16:38:24 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/cristianoidelfonso" target="_blank" rel="noreferrer">
+            <div className="dsmovie-contact-container">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/cristianoidelfonso</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
