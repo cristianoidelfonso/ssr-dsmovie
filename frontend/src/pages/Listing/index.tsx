5bc70cdd9575f71f4fad3aff25eb46ebcd007fc3
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-01-15 10:39:48 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,17 +2,43 @@ import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-  // Forma errada
-  axios.get(`${BASE_URL}/movies?size=4&page=0`)
+  // useState
+  const [pageNumber, setPageNumber] = useState(0);
+
+  // Forma correta: fazendo a requisição dentro de uma hook
+  // useEffect
+  useEffect(() => {
+      axios.get(`${BASE_URL}/movies?size=4&page=1`)
         .then(response => {
           console.log(response.data);
-        });
+          const data = response.data as MoviePage;
+          setPageNumber(data.number);
+        }
+      );
+    }, 
+    []
+  );
+
+
+
+  // Forma errada: deixando a requisição solta no ciclo de vida do componente
+  // axios.get(`${BASE_URL}/movies?size=4&page=0`)
+        // .then(response => {
+          // console.log(response.data);
+          // const data = response.data as MoviePage;
+          // setPageNumber(data.number);
+        // });
+
 
   return(
     <>
+      <p>{pageNumber}</p>
+
       <Pagination />
 
       <div className="container">
