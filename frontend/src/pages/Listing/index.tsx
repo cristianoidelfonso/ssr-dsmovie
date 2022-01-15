2b096959f4daaf81c18965de6aac4b4ed701a43a
Cristiano Idelfonso <cristiano_idelfonso@hotmail.com>
2022-01-15 11:05:34 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,67 +9,64 @@ function Listing() {
 
   // useState
   const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>(
+    {
+      content: [],
+      last: true,
+      totalPages: 0,
+      totalElements: 0,
+      size: 8,
+      number: 0,
+      first: true,
+      numberOfElements: 0,
+      empty: true,
+    }
+  );
 
   // Forma correta: fazendo a requisição dentro de uma hook
   // useEffect
   useEffect(() => {
-      axios.get(`${BASE_URL}/movies?size=4&page=1`)
-        .then(response => {
-          console.log(response.data);
-          const data = response.data as MoviePage;
-          setPageNumber(data.number);
-        }
+    axios.get(`${BASE_URL}/movies?size=${page.size}&page=${pageNumber}`)
+      .then(response => {
+        // console.log(response.data);
+        // setPageNumber(data.number);
+        const data = response.data as MoviePage;
+        setPage(data);
+      }
       );
-    }, 
-    []
+  },
+    [pageNumber]
   );
-
-
 
   // Forma errada: deixando a requisição solta no ciclo de vida do componente
   // axios.get(`${BASE_URL}/movies?size=4&page=0`)
-        // .then(response => {
-          // console.log(response.data);
-          // const data = response.data as MoviePage;
-          // setPageNumber(data.number);
-        // });
+  // .then(response => {
+  // console.log(response.data);
+  // const data = response.data as MoviePage;
+  // setPageNumber(data.number);
+  // });
 
 
-  return(
+  return (
     <>
-      <p>{pageNumber}</p>
-
       <Pagination />
 
       <div className="container">
         <div className="row">
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
-          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard />
-          </div>
+
+          {page.content.map(item => {
+                return (
+                  <div key={item.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <MovieCard movie={item} />
+                  </div>
+                )
+              }
+            )
+          }
+
         </div>
       </div>
-      
+
     </>
 
   );
